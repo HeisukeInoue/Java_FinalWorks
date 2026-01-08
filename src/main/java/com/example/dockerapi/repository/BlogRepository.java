919e8de518ff6e06ed2e39b7d556c4b2dac13267
HeisukeInoue <kenbahn2002@gmail.com>
2026-01-08 17:26:43 +0900

    ブログリスト取得APIの実装

--- src/main/java/com/example/dockerapi/repository/BlogRepository.java
+++ src/main/java/com/example/dockerapi/repository/BlogRepository.java
@@ -1,5 +1,6 @@
 package com.example.dockerapi.repository; //このクラスが属する**パッケージ（フォルダ階層）**の宣言。
 
+import java.util.List;
 /*import org.springframework.beans.factory.annotation.Autowired;*/ //フィールド注入は非推奨
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -23,17 +24,52 @@ public class BlogRepository {
             WHERE id = ? AND deleted_at IS NULL;
             """;
         
-            RowMapper<Blog> mapper = (rs, rowNum) -> new Blog(
-              rs.getInt("id"),
-              rs.getInt("talent_id"),
-              rs.getString("title"),
-              rs.getString("text"),
-              rs.getTimestamp("created_at") != null ? rs.getTimestamp("created_at").toLocalDateTime() : null,
-              rs.getTimestamp("updated_at") != null ? rs.getTimestamp("updated_at").toLocalDateTime() : null,
-              rs.getTimestamp("deleted_at") != null ? rs.getTimestamp("deleted_at").toLocalDateTime() : null
-          );
+          RowMapper<Blog> mapper = (rs, rowNum) -> new Blog(
+            rs.getInt("id"),
+            rs.getInt("talent_id"),
+            rs.getString("title"),
+            rs.getString("text"),
+            rs.getTimestamp("created_at") != null ? rs.getTimestamp("created_at").toLocalDateTime() : null,
+            rs.getTimestamp("updated_at") != null ? rs.getTimestamp("updated_at").toLocalDateTime() : null,
+            rs.getTimestamp("deleted_at") != null ? rs.getTimestamp("deleted_at").toLocalDateTime() : null
+        );
+    
+        return jdbcTemplate.queryForObject(sql, mapper, id);
+    }
+
+    /*指定のあったページindexをもとにブログ記事リストを取得する*/
+    public List<Blog> getBlogsByCurrentPage(int pageSize, int offSet) {
+      String sql = """
+          SELECT id, talent_id, title, text, created_at, updated_at, deleted_at
+          FROM blogs
+          WHERE deleted_at IS NULL
+          ORDER BY created_at DESC
+          LIMIT ?
+          OFFSET ?
+          """;
       
-          return jdbcTemplate.queryForObject(sql, mapper, id);
+
+      RowMapper<Blog> mapper = (rs, rowNum) -> new Blog(
+          rs.getInt("id"),
+          rs.getInt("talent_id"),
+          rs.getString("title"),
+          rs.getString("text"),
+          rs.getTimestamp("created_at") != null ? rs.getTimestamp("created_at").toLocalDateTime() : null,
+          rs.getTimestamp("updated_at") != null ? rs.getTimestamp("updated_at").toLocalDateTime() : null,
+          rs.getTimestamp("deleted_at") != null ? rs.getTimestamp("deleted_at").toLocalDateTime() : null
+      );
+      
+      return jdbcTemplate.query(sql, mapper, pageSize, offSet);    
+    }
+
+    /*ブログ総件数を取得する*/
+    public Long getTotalBlogCounts() {
+      String sql = """
+          SELECT COUNT(*)
+          FROM blogs
+          WHERE deleted_at IS NULL
+          """;
+      return jdbcTemplate.queryForObject(sql, Long.class);    
     }
 
     /*個別にブログ記事を削除する*/
