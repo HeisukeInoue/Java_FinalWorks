eba05f6abbbf5e39c98c9b1d55b30104ca479741
HeisukeInoue <kenbahn2002@gmail.com>
2026-01-14 11:18:55 +0900

    出演情報投稿API実装

--- src/main/java/com/example/dockerapi/controller/AppearanceController.java
+++ src/main/java/com/example/dockerapi/controller/AppearanceController.java
@@ -1,21 +1,18 @@
 package com.example.dockerapi.controller;
+
+import java.sql.Date;
 import java.util.List;
-// import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.dockerapi.dto.AppearanceRequest;
 import com.example.dockerapi.model.Appearance;
-// import com.example.dockerapi.dto.BlogListResponse;
-// import com.example.dockerapi.model.Blog;
-import com.example.dockerapi.repository.AppearanceRepository;
+import com.example.dockerapi.model.AppearanceDetail;
 import com.example.dockerapi.service.AppearanceService;
 
 @RestController
@@ -23,16 +20,41 @@ import com.example.dockerapi.service.AppearanceService;
 public class AppearanceController {
 
     @Autowired
-        private AppearanceService AppearanceService;
-    @Autowired
-        private AppearanceRepository AppearanceRepository;
+    private  AppearanceService appearanceService;
 
     /*タレント出演情報の一覧を取得する*/
     @GetMapping
     public ResponseEntity<?> getAllAppearance() {
         try {
-            List<Appearance> appearance = AppearanceService.getAllAppearance();
+            List<Appearance> appearance = appearanceService.getAllAppearance();
             return ResponseEntity.ok(appearance);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("エラーが発生しました");
+        }
+    }
+
+    /*タレント出演情報の詳細を取得する*/
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAppearanceDetail(@PathVariable int id) {
+        try {
+            AppearanceDetail detail = appearanceService.getAppearanceDetail(id);
+            return ResponseEntity.ok(detail);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("エラーが発生しました");
+        }
+    }
+
+    /*タレント出演情報を投稿する*/
+    @PostMapping
+    public ResponseEntity<?> postNewAppearance(@RequestBody AppearanceRequest request) { //リクエストをdto"AppearanceRequest"へ
+        try {
+            Date date = request.getDate();
+            String title = request.getTitle();
+            String text = request.getText();
+            appearanceService.postNewAppearance(date, title, text);
+            return ResponseEntity.status(201).body("出演情報を登録しました");
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("エラーが発生しました");
